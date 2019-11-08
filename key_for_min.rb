2bb5e560fac7903186fa10708d9d6a2a95d94c71
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-08 16:18:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = "bananas"
+  name_hash.map do |name, age|
+    if min == "bananas"
+      min = age 
+    else 
+      if min > age 
+        min = age 
+        key = name
+      end 
+    end
+  end 
+  key
 end
